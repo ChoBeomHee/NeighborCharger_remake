f367bdd4d34b4fe7912f0ef739f51738a6e4afed
CheBeomHee <tigerfrom2@naver.com>
2025-03-11 22:42:32 +0900

    (feat) make git repository

---
+++ src/test/java/project/charger/ChargerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.charger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
